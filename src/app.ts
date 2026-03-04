e9ed297c9dc6692fcdfbac9d42117747aa5bfa09
gaffarsweb <138963075+gaffarsweb@users.noreply.github.com>
2026-03-04 20:36:33 +0530

    upa

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,12 @@ import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true, // allow all origins dynamically
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
