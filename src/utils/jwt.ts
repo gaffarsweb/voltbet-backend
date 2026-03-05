476832df96cbaa3458eb1e0cae3748e55bf4de8e
gaffarsweb <138963075+gaffarsweb@users.noreply.github.com>
2026-03-05 09:03:39 +0530

    update

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const ACCESS_SECRET = process.env.JWT_SECRET!;
-const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
+const ACCESS_SECRET = process.env.JWT_SECRET || "default_access_secret";
+const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "default_refresh_secret";
 
 export const generateAccessToken = (payload: any) => {
   return jwt.sign(payload, ACCESS_SECRET, {
