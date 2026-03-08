c4f203ecd8cec13b71a85accd113cc6bbf3ff2c9
gaffarsweb <138963075+gaffarsweb@users.noreply.github.com>
2026-03-08 11:52:03 +0530

    redis added

--- src/controllers/balance/balance.controller.ts
+++ src/controllers/balance/balance.controller.ts
@@ -2,13 +2,39 @@ import mongoose from "mongoose";
 import walletModel from "../../models/wallet.model";
 import { errorResponse, successResponse } from "../../utils/response";
 import { Request, Response } from "express";
+import { redisClient } from "../../config/redis";
 
 export const getBalance = async (req: Request, res: Response) => {
-    const user = await walletModel.findOne({userId:new mongoose.Types.ObjectId(req?.user?.id)});
-    if (!user) {
-        return errorResponse(res, "Wallet not found");
-    }
-    return successResponse(res, "Balance fetched successfully", {
-        balance: user.balance.toString(),
+  const userId = req?.user?.id;
+
+  const cacheKey = `balance:${userId}`;
+
+  // 1️⃣ Check Redis first
+  const cachedBalance = await redisClient.get(cacheKey);
+
+  if (cachedBalance) {
+    return successResponse(res, "Balance fetched from cache", {
+      balance: cachedBalance,
     });
+  }
+
+  // 2️⃣ If not in cache → fetch from DB
+  const user = await walletModel.findOne({
+    userId: new mongoose.Types.ObjectId(userId),
+  });
+
+  if (!user) {
+    return errorResponse(res, "Wallet not found");
+  }
+
+  const balance = user.balance.toString();
+
+  // 3️⃣ Store in Redis for 5 hours
+  await redisClient.set(cacheKey, balance, {
+    EX: 60 * 60 * 5, // 5 hours
+  });
+
+  return successResponse(res, "Balance fetched successfully", {
+    balance,
+  });
 };
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import app from "./app";
 import { connectDB } from "./config/db";
 import { initSocket } from "./socket/socket";
 import { setIO } from "./socket/socketInstance";
-
+import { connectRedis } from "./config/redis";
 dotenv.config();
 
 const startServer = async () => {
     await connectDB();
 
     const server = http.createServer(app);
+    await  connectRedis();
     const io = await initSocket(server);
     setIO(io);
     server.listen(process.env.PORT, () => {
